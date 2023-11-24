60206c94c4ab42ea261af5f7e198fbeb2d75b310
allen-teta <148436556+allen-teta@users.noreply.github.com>
2023-11-24 12:06:45 +0200

    first commit

--- vowel.js
+++ vowel.js
@@ -1,11 +1,14 @@
 function vol(name){
     var voewel =[ 'a','e','i','o','u'];
-     var count=name.split();
-    for(i=name;name.length;i++){
+     var count=0;
+    for(i=0;i<name.length;i++){
+        if (voewel.includes(name[i])) {
+            count++
+        }
         
-        return count;
+        
     }
-    
+    return count;
     
     }
     console.log(vol("hello"));
